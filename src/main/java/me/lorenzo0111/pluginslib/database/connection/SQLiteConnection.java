c8d0d9741d832b55e293dd0f3925d8cb8ec4da48
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2021-06-16 19:42:18 +0200

    Added missing throws

--- src/main/java/me/lorenzo0111/pluginslib/database/connection/SQLiteConnection.java
+++ src/main/java/me/lorenzo0111/pluginslib/database/connection/SQLiteConnection.java
@@ -3,6 +3,7 @@ package me.lorenzo0111.pluginslib.database.connection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,7 +13,7 @@ public class SQLiteConnection extends JavaConnection {
      * @param plugin Owner of the sqlite database
      * @throws SQLException if something went wrong
      */
-    public SQLiteConnection(JavaPlugin plugin) throws SQLException {
+    public SQLiteConnection(JavaPlugin plugin) throws SQLException, IOException {
         this(new File(plugin.getDataFolder(),"database.db"));
     }
 
@@ -20,7 +21,7 @@ public class SQLiteConnection extends JavaConnection {
      * @param file SQLite database file
      * @throws SQLException if something went wrong
      */
-    public SQLiteConnection(File file) throws SQLException {
+    public SQLiteConnection(File file) throws SQLException, IOException {
         this(create(file));
     }
 
@@ -31,7 +32,7 @@ public class SQLiteConnection extends JavaConnection {
         super(connection);
     }
 
-    private static Connection create(File file) throws SQLException {
+    private static Connection create(File file) throws SQLException, IOException {
         file.getParentFile().mkdirs();
         file.createNewFile();
         return DriverManager.getConnection("jdbc:sqlite:" + file.getAbsolutePath());
